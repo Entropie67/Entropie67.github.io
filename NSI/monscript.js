2fdccdce0a9c3b3eb0b905af3302793293c3b7a6
Olivier <olivier.elophe@gmail.com>
2020-12-01 14:23:03 +0100

    réccupération de input

--- NSI/monscript.js
+++ NSI/monscript.js
@@ -1,17 +1,34 @@
 
-alert("Coucou");
+console.log("Ici c'est la console rpz")
 
-console.log("Coucou secret")
-/*
-for (let pas = 0; pas < 12; pas++) {
-    document.write("Hello world !", pas);
-    console.log(pas)
-}
-*/
 const element = document.querySelector("#premier");
+const button = document.querySelector("button");
+const valide = document.querySelector("#valide");
+
+
+
+
+valide.addEventListener('click', function(event){
+    const nom = document.querySelector("input").value;
+    alert("Salut " + nom);
+    console.log(nom);
+});
+
+
+
+button.addEventListener('click', function(event){
+
+    console.log("Click !");
+    element.style.display = "none";
+    event.preventDefault();
+});
+
 
 element.addEventListener('click', function() {
     element.style.color = "#F00";
-})
+});
+
+
+
 
 console.log(element.textContent);
