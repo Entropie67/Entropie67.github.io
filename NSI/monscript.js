828c3133cbc50cea12a048a36ab0bac5efee0118
Olivier <olivier.elophe@gmail.com>
2020-12-01 15:01:34 +0100

    début du combat

--- NSI/monscript.js
+++ NSI/monscript.js
@@ -4,32 +4,14 @@ console.log("Ici c'est la console rpz")
 const element = document.querySelector("#premier");
 const button = document.querySelector("button");
 const valide = document.querySelector("#valide");
+const combat = document.querySelector("#combat");
+const hajime = document.querySelector("#hajime");
 
-
-
-
-valide.addEventListener('click', function(event){
-    const nom = document.querySelector("input").value;
-    alert("Salut " + nom);
-
-    console.log(nom);
+hajime.addEventListener('click', function (ev) {
+    const nomPlayer1 = document.querySelector("#player1").value;
+    const nomPlayer2 = document.querySelector("#player2").value;
+    combat.style.display = "block";
+    alert(nomPlayer1 + " VS " + nomPlayer2);
 });
 
 
-
-button.addEventListener('click', function(event){
-
-    console.log("Click !");
-    element.style.display = "none";
-    event.preventDefault();
-});
-
-
-element.addEventListener('click', function() {
-    element.style.color = "#F00";
-});
-
-
-
-
-console.log(element.textContent);
