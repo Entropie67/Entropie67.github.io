e7b116e1964e08c5ed4f3952ef64ee0eba81161f
Entropie67 <olivier.elophe@gmail.com>
2023-10-18 10:47:58 +0200

    art et numérique  (rename)

--- Art_et_Numerique/sketch.js
+++ Art_et_Numerique/sketch.js
@@ -1,16 +1,27 @@
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(720, 560);
+    noStroke();
+    noLoop();
 }
 
 function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+    drawCircle(width / 2, 280, 6);
+}
 
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+function drawCircle(x, radius, level) {
+    // 'level' is the variable that terminates the recursion once it reaches
+    // a certain value (here, 1). If a terminating condition is not
+    // specified, a recursive function keeps calling itself again and again
+    // until it runs out of stack space - not a favourable outcome!
+    const tt = (126 * level) / 4.0;
+    fill(tt);
+    ellipse(x, height / 2, radius * 2, radius * 2);
+    if (level > 1) {
+        // 'level' decreases by 1 at every step and thus makes the terminating condition
+        // attainable
+        level = level - 1;
+        drawCircle(x - radius / 2, radius / 2, level);
+        drawCircle(x + radius / 2, radius / 2, level);
     }
-    ellipse(mouseX, mouseY, 80, 80);
-
 }
