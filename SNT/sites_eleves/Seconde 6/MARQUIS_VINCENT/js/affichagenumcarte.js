b991b5d38d32c5ddae484112ca71b2a77a96e8d7
Entropie67 <olivier.elophe@gmail.com>
2024-02-06 23:11:31 +0100

    2024

--- SNT/sites_eleves/Seconde 6/MARQUIS_VINCENT/js/affichagenumcarte.js
+++
@@ -1,6 +0,0 @@
-$('#inputc').on('input', function (e) {
-    $("#numcarte").html($(this).val());
-});
-document.getElementById('inputc').addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
-});
